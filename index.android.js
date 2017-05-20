1e2d6f3af0b153b1e81e82a75a5c3eed10dc9a74
Audrey Gehring <audreygehring@gmail.com>
2017-05-20 16:03:05 -0400

    bird sprite

--- index.android.js
+++ index.android.js
@@ -9,23 +9,97 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
 } from 'react-native';
 
-export default class TestProject extends Component {
+import AnimatedSprite from 'react-native-animated-sprite';
+import AnimatedSpriteMatrix from 'rn-animated-sprite-matrix';
+
+import _ from 'lodash';
+import cellSprite from "./sprites/button/buttonCharacter";
+import randomstring from 'random-string';
+import birdSprite from './sprites/bird/birdCharacter';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
+export default class matrix extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: [],
+    }
+    this.activeCells =  [true, true, true, true, true, true, true, true, true, true, true, true];
+		this.animationKeys = ["WALK", "IDLE", "IDLE", "EAT", "IDLE", "IDLE", "IDLE", "IDLE", "IDLE", "IDLE", "IDLE", "IDLE"];
+    this.loopAnimation = _.fill(Array(this.activeCells.length), false);
+    this.sprites = _.fill(Array(this.activeCells.length), birdSprite);
+    this.scale = {image: 1};
+    this.cellSpriteScale = 0.5;
+    this.numColumns = 4;
+    this.numRows = 3;
+  }
+
+  componentWillMount () {
+    this.setState({cells: this.createCellObjsArray()});
+  }
+
+  createCellObjsArray () {
+    const cells = _.map(this.activeCells , (active, index) => ({
+      sprite: this.sprites[index],
+      animationKey: this.animationKeys[index],
+      loopAnimation: this.loopAnimation[index],
+      uid: randomstring({ length: 7 }),
+      active,
+    }));
+    return cells;
+  }
+
+  matrixLocation () {
+    const size = birdSprite.size;
+    const width = this.numColumns * size.width * this.cellSpriteScale;
+    const height = this.numRows * size.height * this.cellSpriteScale;
+    const top = screenHeight / 2 - height/2;
+    const left = screenWidth / 2 - width/2;
+    const location = {top, left};
+    return location;
+  }
+  matrixSize () {
+    const size = birdSprite.size;
+    const width = this.numColumns * size.width * this.cellSpriteScale;
+    const height = this.numRows * size.height * this.cellSpriteScale;
+    return {width, height};
+  }
+
+  cellPressed (cellObj, position) {
+    const cells = _.cloneDeep(this.state.cells);
+    if (cells[position].animationKey === "WALK") {
+      cells[position].animationKey = "EAT";
+    } else {
+      cells[position].animationKey = "WALK";
+    }
+    cells[position].loopAnimation = true;
+    // must change key to force redraw
+    cells[position].uid = randomstring({length: 7});
+    this.setState({cells});
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native, Audrey!! You made it!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
+        <AnimatedSpriteMatrix
+          styles={{
+              ...(this.matrixLocation()),
+              ...(this.matrixSize()),
+              position: 'absolute',
+            }}
+          dimensions={{columns: this.numColumns, rows: this.numRows}}
+          cellSpriteScale={this.cellSpriteScale}
+          cellObjs={this.state.cells}
+          scale={this.scale}
+          onPress={(cellObj, position) => this.cellPressed(cellObj, position)}
+        />
       </View>
     );
   }
@@ -34,8 +108,8 @@ export default class TestProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
@@ -50,4 +124,5 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('TestProject', () => TestProject);
+
+AppRegistry.registerComponent('matrix', () => matrix);
